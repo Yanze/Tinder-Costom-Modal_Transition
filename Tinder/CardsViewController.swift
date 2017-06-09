834e7e058482ace7b1b1fe82bcd3ec10da18c510
Jessica <yanzeliu.france@gmail.com>
2017-06-09 13:01:19 -0700

    custom transition

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class CardsViewController: UIViewController, DraggableImageViewDelegate {
+class CardsViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning, DraggableImageViewDelegate {
     var imageView: DraggableImageView!
     var profileImageOriginalCenter: CGPoint!
+    
+    var isPresenting: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +25,62 @@ class CardsViewController: UIViewController, DraggableImageViewDelegate {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     func imgViewTapped() {
-        let profileVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ProfileViewController") as! ProfileViewController
-        self.present(profileVC, animated: true, completion: nil)
+        performSegue(withIdentifier: "profileSegue", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "profileSegue" {
+            let destionationVC = segue.destination as! ProfileViewController
+            destionationVC.modalPresentationStyle = UIModalPresentationStyle.custom
+            destionationVC.transitioningDelegate = self
+        }
+    }
+    
+    //MARK: transition methods
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = true
+        return self
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = false
+        return self
+    }
+    
+    // MARK: methods that actually control the transition
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        // this value is the duration of the animations scheduled in the animationTransition method
+        return 0.4
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!
+        let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
+        
+        if isPresenting {
+            containerView.addSubview(toViewController.view)
+            toViewController.view.alpha = 0
+            UIView.animate(withDuration: 0.4, animations: { 
+                toViewController.view.alpha = 1
+            }, completion: { (finished:Bool) in
+                transitionContext.completeTransition(true)
+            })
+        }
+        else {
+            UIView.animate(withDuration: 0.4, animations: { 
+                fromViewController.view.alpha = 0
+            }, completion: { (finished:Bool) in
+                transitionContext.completeTransition(true)
+                fromViewController.view.removeFromSuperview()
+            })
+        }
+        
     }
 
 }
+
 
